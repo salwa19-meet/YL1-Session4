fc00f0cdcc00fc0c6c01a5b0440cca57ac0635a7
shiraz19-meet <shiraz19@meet.mit.edu>
2017-11-06 19:22:09 +0200

    uploaded code

---
+++ lab.py
@@ -0,0 +1,14 @@
+class Animal(object):
+	def __init__(self,sound,name,age,favorite_color):
+		self.sound = sound
+		self.name = name
+		self.age = age
+		self.favorite_color = favorite_color
+	def eat(self.food):
+			print("Yummy !! " + self.name + " is eating " + food)
+	def description(self):
+		print(self.name + " is " + self.age +" years old and loves the color "+self.favorite_color)
+    def make_sound(self.sound):
+    	print(self.sound*3)
+salwa = Animal("hhaha", "salwa", 15, "blue")
+print (salwa.name)
